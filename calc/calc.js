db3b100b9d8d2ddf2db63c55608cff863389a9f1
15029 <cameronweiss021@gmail.com>
2025-12-11 11:03:24 -0500

    Javascript yes

--- calc/calc.js
+++ calc/calc.js
@@ -6,3 +6,25 @@ buttons.forEach(
         button.addEventListener("click",calculate);
     }
 );
+
+function calculate(event){
+    //What was clicked
+    const clickedValue =  event.target.value;
+
+    // Handle what was clicked
+    //If it's an equal sign
+    if(clickedValue === "="){
+        //Is the display empty
+        if(display.value !== ""){
+            //Calculate the value and display
+            display.value = eval(display.value);
+        }
+    }else if(clickedValue === "C"){
+        //Empty the display
+        display.value = "";
+    }
+    //Otherwise add it to the display
+    else{
+        display.value += clickedValue;
+    }
+}
